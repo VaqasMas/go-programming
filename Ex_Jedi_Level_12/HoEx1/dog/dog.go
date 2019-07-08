f0e26a29abae42f358e8ad0baff916d65e55468f
Syed Masood | MachinesTalk <3199@FHI.Group>
2019-07-08 22:49:59 +0300

    Ex Jedi Level 12

---
+++ Ex_Jedi_Level_12/HoEx1/dog/dog.go
@@ -0,0 +1,9 @@
+//Package dog takes the human year and convert them to dog years.
+package dog
+
+const dyear = 7
+
+//Years function is used for converting human year to dog years
+func Years(y int) int {
+	return y * dyear
+}
---
+++ Ex_Jedi_Level_12/HoEx1/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/VaqasMas/go-programming/Ex_Jedi_Level_12/HoEx1/dog"
+)
+
+func main() {
+
+	hyear := 2
+
+	dogyear := dog.Years(hyear)
+
+	fmt.Println("Dog year = ", dogyear)
+
+}
